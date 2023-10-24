b151e62abf6f140abb1abed4c861daad969bbf06
Gregory Tucker <gregory.tucker@ess.eu>
2023-10-24 13:02:10 +0200

    [Add] function help [Fix] cibuildwheel macos target

--- src/chopcal.cpp
+++ src/chopcal.cpp
@@ -8,6 +8,10 @@
 
 #include "choppers.h"
 
+namespace nb = nanobind;
+using namespace nb::literals;
+
 NB_MODULE(_chopcal_impl, m) {
-m.def("bifrost", &bifrost);
+m.def("bifrost", &bifrost, "e_max"_a, "lambda_min"_a=0, "shaping_time"_a=0.0002,
+      "Calculate chopper parameters for the BIFROST spectrometer. Each of (phase, delay, phase) for the six choppers.");
 }
